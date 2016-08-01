95b783eb3d154e92d43d5cae6a47ada06e54c444
David Somers <david@jalada.co.uk>
2016-08-01 13:34:36 +0100

    I forgot a configuration accessor

--- lib/letsencrypt-rails-heroku/letsencrypt.rb
+++ lib/letsencrypt-rails-heroku/letsencrypt.rb
@@ -15,7 +15,7 @@ module Letsencrypt
   end
 
   class Configuration
-    attr_accessor :heroku_token, :heroku_app, :acme_email, :acme_domain
+    attr_accessor :heroku_token, :heroku_app, :acme_email, :acme_domain, :acme_endpoint
     
     # Not settable by user; part of the gem's behaviour.
     attr_reader :acme_challenge_filename, :acme_challenge_file_content
