00e6a05fde4025536cfe0f9177d9c86ef53d2f5f
Cristiano Betta <cristiano@betta.io>
2016-10-28 14:32:06 +0100

    Update gem to not use any Rails specific code

--- lib/letsencrypt-rails-heroku/letsencrypt.rb
+++ lib/letsencrypt-rails-heroku/letsencrypt.rb
@@ -9,9 +9,9 @@ module Letsencrypt
   end
 
   def self.challenge_configured?
-    configuration.acme_challenge_filename.present? && 
-      configuration.acme_challenge_filename.starts_with?(".well-known/") &&
-      configuration.acme_challenge_file_content.present?
+    configuration.acme_challenge_filename != nil && 
+      configuration.acme_challenge_filename.start_with?(".well-known/") &&
+      configuration.acme_challenge_file_content != nil
   end
 
   class Configuration
@@ -25,14 +25,14 @@ module Letsencrypt
       @heroku_app = ENV["HEROKU_APP"]
       @acme_email = ENV["ACME_EMAIL"]
       @acme_domain = ENV["ACME_DOMAIN"]
-      @acme_endpoint = ENV["ACME_ENDPOINT"].presence || 'https://acme-v01.api.letsencrypt.org/'
+      @acme_endpoint = (ENV["ACME_ENDPOINT"] != nil ? ENV["ACME_ENDPOINT"] : nil) || 'https://acme-v01.api.letsencrypt.org/'
       @acme_challenge_filename = ENV["ACME_CHALLENGE_FILENAME"]
       @acme_challenge_file_content = ENV["ACME_CHALLENGE_FILE_CONTENT"]
     end
 
     def valid?
-      heroku_token.present? && heroku_app.present? && acme_email.present? &&
-        acme_domain.present?
+      heroku_token != nil && heroku_app != nil && acme_email != nil &&
+        acme_domain != nil
     end
   end
 end
--- lib/tasks/letsencrypt.rake
+++ lib/tasks/letsencrypt.rake
@@ -6,7 +6,7 @@ require 'platform-api'
 namespace :letsencrypt do
 
   desc 'Renew your LetsEncrypt certificate'
-  task :renew => :environment do
+  task :renew do
     # Check configuration looks OK
     abort "letsencrypt-rails-heroku is configured incorrectly. Are you missing an environment variable or other configuration? You should have a heroku_token, heroku_app, acmp_email and acme_domain configured either via a `Letsencrypt.configure` block in an initializer or as environment variables." unless Letsencrypt.configuration.valid?
 
