5a4c0feee10c4d87a22e78b65a77349a304e0f3c
Curtis Parfitt-Ford <curtis@mashedkeyboard.me>
2019-02-16 09:47:31 +0000

    VerifyWith needs to be required(!)

--- lib/letsencrypt-rails-heroku.rb
+++ lib/letsencrypt-rails-heroku.rb
@@ -1,6 +1,7 @@
 require 'letsencrypt-rails-heroku/letsencrypt'
 require 'letsencrypt-rails-heroku/middleware'
 require 'letsencrypt-rails-heroku/exceptions'
+require 'letsencrypt-rails-heroku/verify_with'
 
 if defined?(Rails)
   require 'letsencrypt-rails-heroku/railtie'
